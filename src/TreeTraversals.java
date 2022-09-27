6d3e9361db0fb20c349d19a5ce6ad29da1f9ea80
My Name <zuberkhan0808@gmail.com>
2022-09-27 10:38:03 +0530

    make printFunction of tree

--- src/TreeTraversals.java
+++ src/TreeTraversals.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 class Node{
   int data;
@@ -36,20 +37,66 @@ class TreeTraversals{
     }
   }
 
+  public static void printTree(){
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+    while(!queue.isEmpty()){
+      Node temp = queue.poll();
+      System.out.print(temp.data+" ");
+      if(temp.left != null) queue.add(temp.left);
+      if(temp.right != null) queue.add(temp.right);
+    }
+  }
+
+
+   // inserting node in the tree
+  public static void insertNode(int data){
+     Node newNode = new Node(data);
+     if(root == null){
+        root = newNode;
+        return;
+     }
+
+     Queue<Node> queue = new LinkedList<>();
+     queue.add(root);
+
+     Node temp;
+
+     while(!queue.isEmpty()){
+        temp = queue.poll();
+        if(temp.left == null){
+          temp.left = newNode;
+          break;
+        }else{
+          queue.add(temp.left);
+        }
+
+        if(temp.right == null){
+          temp.right = newNode;
+          break;
+        }else{
+          queue.add(temp.right);
+        }
+     }
+
+    //  return root;
+  }
+
   public static void main(String[] args) {
    
     TreeTraversals tree = new TreeTraversals();
-    tree.root = new Node(1);
-    tree.root.left = new Node(2);
-    tree.root.right = new Node(3);
-    tree.root.left.left = new Node(4);
-    tree.root.left.right = new Node(5);
+    for(int  i =0; i<10; i++){
+      int random = (int)(Math.random() * 100) + 1;
+      System.out.println("Inserting " + random + " in the tree");
+      tree.insertNode(random);
 
-    tree.preOrder(root);
-    System.out.println("preorder");
-    tree.inOrder(root);
-    System.out.println("inOrder");
+    }
+
+    System.out.println();
+    // tree.printTree();
+    // tree.inOrder(root);
+    // System.out.println("inOrder");
     tree.postOrder(root);
-    System.out.println("postOrder");
+    // System.out.println("postOrder");
   }
 }
